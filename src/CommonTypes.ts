718f4d97f463dacbd7ca3b1e867770a6aeae3c19
Bruno Alves de Faria <bruno.faria@radicalimaging.com>
2022-10-18 10:25:36 -0300

    Adding new IClass type

--- src/CommonTypes.ts
+++ src/CommonTypes.ts
@@ -6,3 +6,18 @@
  * @returns Nothing (handler functions shall have no return value)
  */
 export type Handler = <T>(...t: T[]) => void;
+
+/**
+ * Generic Class interface to be used as type.
+ *
+ * @param <T> - Generic type for defining the abstract class to be bridged
+ */
+export interface IClass<T> {
+  /**
+   * Generic constructor definition for the bridged class
+   *
+   * @param r - Rest parameter with the entire constructor payload
+   * @returns A bridged class' instance
+   */
+  new <R>(...r: R[]): T;
+}
